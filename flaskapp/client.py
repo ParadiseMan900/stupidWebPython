3f5d7fc0cc6d3ed798594b1c544dc094243ee6eb
ParadiseMan900 <119621338+ParadiseMan900@users.noreply.github.com>
2023-05-10 19:50:48 +0700

    Delete flaskapp directory

--- flaskapp/client.py
+++
@@ -1,4 +0,0 @@
-import requests
-r = requests.get('http://localhost:1234/')
-print(r.status_code)
-print(r.text)
--- flaskapp/wsgi.py
+++
@@ -1,5 +0,0 @@
-from some_app import app
-
-portIndex = 1234
-if __name__ == "__main__":
-    app.run(port=portIndex)
